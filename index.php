884954c7cc98642d5182e2142b9ca7f5501662e0
LauraJanuskeviciene <laura.januske@gmail.com>
2022-03-03 08:52:41 +0200

    initialize project

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+require 'vendor/autoload.php';
+
+$log = new Monolog\Logger('name');
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+$log->addWarning('Foo');
+
+echo('start');
